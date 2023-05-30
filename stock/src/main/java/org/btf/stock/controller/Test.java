db38a5984dffbb5bb3eeaf2ef67dbb33eecae9f1
btf <1967980236@qq.com>
2023-05-30 17:16:39 +0800

    描述 六

--- stock/src/main/java/org/btf/stock/controller/Test.java
+++ stock/src/main/java/org/btf/stock/controller/Test.java
@@ -2,6 +2,6 @@ package org.btf.stock.controller;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Git  阿萨德测试");
+        System.out.println("Git 测试qweq");
     }
 }
